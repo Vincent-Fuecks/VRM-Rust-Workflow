dacdf68fc949f3285443a847dd83f10b68e0b8bf
Vincent Fücks <fuev99@zedat.fu-berlin.de>
2025-11-24 19:13:01 +0100

    Update SchedulerType

--- src/domain/vrm_system_model/scheduler_type.rs
+++ src/domain/vrm_system_model/scheduler_type.rs
@@ -21,37 +21,21 @@ impl SchedulerType {
     ) -> Box<dyn Schedule> {
         match self {
             SchedulerType::FreeListSchedule => {
-                // Box::new(FreeListSchedule::new(...))
                 todo!()
             }
             SchedulerType::SlottedSchedule => {
                 todo!()
-                // Box::new(SlottedSchedule::new(
-                // name,
-                // number_of_slots,
-                // slot_width,
-                // capacity,
-                // )),
             }
             SchedulerType::SlottedSchedule12 => {
                 todo!()
-                // let scaled_slots = (number_of_slots * (slot_width + 11)) / 12;
-                // Box::new(SlottedSchedule::new(name, scaled_slots, 12, capacity))
             }
             SchedulerType::SlottedSchedule12000 => {
                 todo!()
-                // let scaled_slots = (number_of_slots * (slot_width + 11999)) / 12000;
-                // Box::new(SlottedSchedule::new(name, scaled_slots, 12000, capacity))
             }
             SchedulerType::SlottedScheduleResubmitFrag => {
                 todo!()
-                // let mut result = SlottedSchedule::new(name, number_of_slots, slot_width, capacity);
-                // result.set_quadratic_mean_fragmentation_calculation(false);
-                // Box::new(result)
             }
             SchedulerType::UnlimitedSchedule => {
-                // Return a concrete UnlimitedSchedule struct
-                // Box::new(UnlimitedSchedule::new(...))
                 todo!()
             }
         }
